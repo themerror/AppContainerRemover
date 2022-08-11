3ac4bcc60011a9234a5c457e0e8e7184ee8aafee
themerror <wu1974692628@outlook.com>
2022-08-11 22:36:14 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+Console.WriteLine("Hello World!");
+
+[DllImport("Userenv.dll", CharSet = CharSet.Unicode)]
+static extern long DeleteAppContainerProfile(StringBuilder appcontainerName);
+
+Console.Write("Enter the name of AppContainer:");
+string name = Console.ReadLine();
+StringBuilder appcontainer = new StringBuilder(name);
+long ret = DeleteAppContainerProfile(appcontainer);
+Console.WriteLine(ret);
