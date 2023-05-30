7ea984906fce54ae4c190fce9a81f372c7f1a3ab
themerror <wu1974692628@outlook.com>
2023-05-30 22:05:04 +0800

    List ACs

---
+++ ConsoleApp1/ListAppContainer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal static class ListAppContainer
+	{
+		public static void List()
+		{
+			RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Mappings");
+
+			string[] subkeyNames = key.GetSubKeyNames();
+
+			foreach (string subkeyName in subkeyNames)
+			{
+				Console.WriteLine(subkeyName);
+				RegistryKey subkey = key.OpenSubKey(subkeyName, true);
+				string[] valueNames = subkey.GetValueNames();
+				foreach (string valueName in valueNames)
+				{
+					Console.WriteLine("  {0} = {1}", valueName, subkey.GetValue(valueName));
+				}
+			}
+		}
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,20 @@
-﻿using System.Runtime.InteropServices;
+﻿using ConsoleApp1;
 using System.Text;
+using static Vanara.PInvoke.UserEnv;
 
-Console.WriteLine("Hello World!");
+Console.WriteLine("Here are all the AppContainers:");
+ListAppContainer.List();
 
-[DllImport("Userenv.dll", CharSet = CharSet.Unicode)]
-static extern long DeleteAppContainerProfile(StringBuilder appcontainerName);
-
-Console.Write("Enter the name of AppContainer:");
-string name = Console.ReadLine();
-StringBuilder appcontainer = new StringBuilder(name);
-long ret = DeleteAppContainerProfile(appcontainer);
-Console.WriteLine(ret);
+while (true)
+{
+	Console.WriteLine("Enter Q to quit.");
+	Console.Write("Enter the name(Monikor) of AppContainer:");
+	string name = Console.ReadLine();
+	if (name.ToUpper() == "Q")
+	{
+		return;
+	}
+	StringBuilder appcontainer = new StringBuilder(name);
+	var ret = DeleteAppContainerProfile(appcontainer.ToString());
+	Console.WriteLine(ret);
+}
